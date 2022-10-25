3c043cb5b97bafdc721d9f8d4b8dab651e9fbe7d
DavidJo84 <112602671+DavidJo84@users.noreply.github.com>
2022-10-25 11:49:01 +0900

    test11

--- Day39/src/test02.java
+++ Day39/src/test02.java
@@ -4,5 +4,6 @@ public class test02 {
 		System.out.println("±êÇãºê ÀßµÇ´Ï??");
 		System.out.println("±êÇãºê ÀßµÇ´Ï??");
 		System.out.println("¿Ö Çª½¬ ¾ÈµÊ??");
+		System.out.println("¿Ö Çª½¬ ¾ÈµÊ??");
 	}
 }
