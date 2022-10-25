18100eabcd632ccad0997d2bc689e43220fcb085
DavidJo84 <112602671+DavidJo84@users.noreply.github.com>
2022-10-25 17:33:20 +0900

    del

--- Day39/src/test.java
+++
@@ -1,9 +0,0 @@
-
-public class test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("하영");
-		System.out.println("왜안되니");
-	}
-}
--- Day39/src/test02.java
+++
@@ -1,9 +0,0 @@
-
-public class test02 {
-	test02(){
-		System.out.println("±êÇãºê ÀßµÇ´Ï??");
-		System.out.println("±êÇãºê ÀßµÇ´Ï??");
-		System.out.println("¿Ö Çª½¬ ¾ÈµÊ??");
-		System.out.println("¿Ö Çª½¬ ¾ÈµÊ??");
-	}
-}
